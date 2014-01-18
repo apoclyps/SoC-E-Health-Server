225e2f60fc35e76b043f4bcb41b37b747fb42e0e
Kyle <kyle90adam@hotmail.com>
2014-01-18 18:56:25 +0000

    RSS Catergory added, may not be needed if we store as Arraylist of strings instead. Further Discussion needed.

--- E-Health Server/src/uk/co/kyleharrison/ehealth/model/pojo/RSSCatergory.java
+++ E-Health Server/src/uk/co/kyleharrison/ehealth/model/pojo/RSSCatergory.java
@@ -2,7 +2,7 @@ package uk.co.kyleharrison.ehealth.model.pojo;
 
 import java.util.ArrayList;
 
-public class RSSCatergory {
+public class RSSCatergory extends RSSBase {
 	private ArrayList <String> yearTitle;
 	private int year;
 	private String group;
