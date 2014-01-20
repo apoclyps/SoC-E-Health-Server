a14d610a824889774f96d1577e770a51e2eb1448
apoclyps <kyle90adam@hotmail.com>
2014-01-20 11:24:22 +0000

    Json document builder updated

--- E-Health Server/src/uk/co/kyleharrison/ehealth/service/jackson/util/JSONDocumentBuilder.java
+++ E-Health Server/src/uk/co/kyleharrison/ehealth/service/jackson/util/JSONDocumentBuilder.java
@@ -1,5 +1,18 @@
 package uk.co.kyleharrison.ehealth.service.jackson.util;
 
+import uk.co.kyleharrison.ehealth.service.jackson.construct.JSONChannel;
+import uk.co.kyleharrison.ehealth.service.jackson.construct.JSONGroup;
+import uk.co.kyleharrison.ehealth.service.jackson.construct.JSONItem;
+
 public class JSONDocumentBuilder {
 
+	private String JSONContent=null;
+	private JSONChannel JSONChannel;
+	private JSONItem JSONItem;
+	private JSONGroup JSONGroup;
+
+	public JSONDocumentBuilder() {
+		super();
+	}
+	
 }
